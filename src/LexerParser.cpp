247b4f7dbf3e4dd7156ad3592022771d1c9cb411
Roman Belyaev <rsbel94@ya.ru>
2024-06-22 14:20:42 +0300

    added missing break operator in operator()

--- src/LexerParser.cpp
+++ src/LexerParser.cpp
@@ -4,21 +4,21 @@
 void LexerParser::operator()(int argc, char** argv)
 {
     if (argc > 1) {     // check count of args
-        if (argv[1][0] == '-') {    // process given key
+        switch (argv[1][0]) {
+        case '-':                    // process given key
             switch (argv[1][1]) {
             case 'a':
                 todo = add; break;
             case 's':
                 todo = set; break;
-            //case 'h':
-            //    todo = help; break;
             case 'm':
-                todo = mark;
+                todo = mark; break;
             default:
                 todo = help;
-            }
+            }; break;
+        default:
+            todo = help;
         }
-        else todo = help;
     }
 
     if (argc > 2 && isdigit(argv[2][0])) num = atoi(argv[2]);
