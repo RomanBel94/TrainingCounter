aaacd13c8be8c4a0474bf659cbd49bb8b1d61909
Roman Belyaev <rsbel94@ya.ru>
2026-01-18 15:39:51 +0300

    added doxygen comments to Version.h

--- src/Version/Version.h
+++ src/Version/Version.h
@@ -1,6 +1,13 @@
-/*
-        CMake configured header.
-        Contains project version.
-*/
+/** @file Version.h
+ *
+ * @brief CMake configured header. Contains app version.
+ */
 
-#define VERSION "2.1.0"
+/** @brief Project version */
+#pragma once
+#ifndef VERSION_H
+
+#define VERSION "2.1.1"
+
+#define VERSION_H
+#endif
